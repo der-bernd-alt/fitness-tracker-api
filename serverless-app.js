d18d1fa5ce675cc4ce0059a7c8ade624375e98df
Bernd Mayinger <bm.info.me@gmail.com>
2024-12-19 05:16:00 +0100

    Also updated the serverless app with the dialect

--- serverless-app.js
+++ serverless-app.js
@@ -19,6 +19,7 @@ async function initDatabase() {
 
     sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect: 'postgres',
+      dialectModule: require('pg'),
       dialectOptions: {
         ssl: {
           require: true,
